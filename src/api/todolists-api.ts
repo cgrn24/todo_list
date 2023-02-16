965a4149e227cd1d042f28bbb4c6e1d14d88142b
cgrn24 <stlhnt42@gmail.com>
2023-02-16 23:33:01 +0700

    api types refactor

--- src/api/todolists-api.ts
+++ src/api/todolists-api.ts
@@ -1,4 +1,5 @@
 import axios, { AxiosResponse } from 'axios'
+import { GetTasksResponse, LoginParamsType, ResponseType, TaskType, TodolistType, UpdateTaskModelType, UserType } from './types'
 
 const instance = axios.create({
   baseURL: 'https://social-network.samuraijs.com/api/1.1/',
@@ -46,80 +47,4 @@ export const authAPI = {
   logout() {
     return instance.delete<ResponseType>('auth/login')
   },
-}
-
-// types
-export type LoginParamsType = {
-  email: string
-  password: string
-  rememberMe?: boolean
-  captcha?: string
-}
-
-export type UserType = {
-  id: string
-  email: string
-  login: string
-}
-
-export type TodolistType = {
-  id: string
-  title: string
-  addedDate: string
-  order: number
-}
-
-export type FieldErrorType = { field: string; error: string }
-
-export type ResponseType<D = {}> = {
-  resultCode: number
-  messages: Array<string>
-  fieldsErrors?: Array<FieldErrorType>
-  data: D
-}
-
-export enum TaskStatuses {
-  New = 0,
-  InProgress = 1,
-  Completed = 2,
-  Draft = 3,
-}
-
-export enum TaskPriorities {
-  Low = 0,
-  Middle = 1,
-  Hi = 2,
-  Urgently = 3,
-  Later = 4,
-}
-export enum Result_code {
-  OK = 0,
-  ERROR = 1,
-  CAPTCHA = 10,
-}
-
-export type TaskType = {
-  description: string
-  title: string
-  status: TaskStatuses
-  priority: TaskPriorities
-  startDate: string
-  deadline: string
-  id: string
-  todoListId: string
-  order: number
-  addedDate: string
-}
-export type UpdateTaskModelType = {
-  title: string
-  description: string
-  status: TaskStatuses
-  priority: TaskPriorities
-  startDate: string
-  deadline: string
-}
-type GetTasksResponse = {
-  error: string | null
-  totalCount: number
-  items: TaskType[]
 }
